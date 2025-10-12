19d52c1d215f430ded0fafaab5beb85eb9d877d9
Samiansari-github <ansarisamiofficial@gmail.com>
2025-10-12 16:22:18 +0530

    added frontend

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -114,7 +114,19 @@ blogRouter.get("/bulk", async (c) => {
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
 
-    const blogs = await prisma.blog.findMany();
+    const blogs = await prisma.blog.findMany({
+        select :{
+            content :true,
+            title: true,
+            id: true,
+            author : {
+                select: {
+                    name : true
+                }
+            }
+
+        }
+    });
 
 
     return c.json({
@@ -138,6 +150,16 @@ blogRouter.get("/:id",async (c) => {
         where :{
             id : Number(id)
         },
+        select: {
+            id : true,
+            title: true,
+            content : true,
+            author : {
+                select : {
+                    name : true
+                }
+            }
+        }
         
     })
     return c.json({
